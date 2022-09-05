ac1bc8d53eb569a1962c019ac05ce9569be8c805
Joshh4993 <joshedwards2k19@gmail.com>
2022-09-05 23:57:20 +0100

    Update scanner.py

--- PortScanner/scanner.py
+++ PortScanner/scanner.py
@@ -1,60 +1,32 @@
+from unittest import result
+from xmlrpc.client import DateTime
+import pyfiglet
+import sys
 import socket
-import argparse
-from tracemalloc import start
-from colorama import init, Fore
-from threading import Thread, Lock
-from queue import Queue
+from datetime import datetime
 
-init()
+ascii_banner = pyfiglet.figlet_format("PORT SCANNER")
+print(ascii_banner)
 
-GREEN = Fore.GREEN
-RED = Fore.RED
-RESET = Fore.RESET
+target = input(str("Target IP: "))
 
-N_THREADS = 200
-q = Queue()
-print_lock = Lock()
+print("-" * 50)
+print("Scanning Target: " + target)
+print("Scan started at: " + str(datetime.now()))
+print("-" * 50)
 
-def port_scan(port):
-    try:
-        s = socket.socket()
-        s.connect((host, port))
-    except:
+try:
+    for port in range(1,65535):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(0.5)
+
+        result = s.connect_ex((target, port))
+        if result == 0:
+            print("[*] Port {} is open".format(port))
         s.close()
-    else:
-        with print_lock:
-            print(f"{GREEN}[!] {host:15}:{port:15} is open    {RESET}")
-    finally:
-        s.close()
-
-def scan_thread():
-    global q
-    while True:
-        worker = q.get()
-        port_scan(worker)
-        q.task_done()
-
-def main(host, ports):
-    global q
-    for t in range(N_THREADS):
-        t = Thread(target=scan_thread)
-        t.daemon = True
-        t.start()
-    for worker in ports:
-        q.put(worker)
-    q.join()
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Port Scanner")
-    parser.add_argument("host", help="Host to scan.")
-    parser.add_argument("--ports", "-p", dest="port_range", default="1-65535", help="Port range to scan, default range is 1-65535 (all ports)")
-    args = parser.parse_args()
-    host, port_range = args.host, args.port_range
-
-    start_port, end_port = port_range.split("-")
-    start_port, end_port = int(start_port), int(end_port)
-
-    ports = [ p for p in range(start_port, end_port)]
-
-    main(host, ports)
+except KeyboardInterrupt:
+    print("\n Exiting")
+    sys.exit()
+except socket.error:
+    print("\n Host not responding")
+    sys.exit()
